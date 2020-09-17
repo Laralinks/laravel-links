68e4ca113f03eaa117c793e71d9281e1dc0c6a81
Laralinks <richard@laravel.link>
2020-09-17 14:48:16 -0400

    Vision

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,8 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
     require('postcss-import'),
     require('tailwindcss'),
-]);
+]).purgeCss({
+    enabled: mix.inProduction(),
+    folders: ['src', 'templates', 'resources/views'],
+    extensions: ['html', 'js', 'php', 'vue'],
+});
