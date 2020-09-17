39b5e4664a8ae4aa9da4d5eca4586afd25dca8e1
Laralinks <richard@laravel.link>
2020-09-17 15:02:43 -0400

    Removed purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,8 +14,4 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
     require('postcss-import'),
     require('tailwindcss'),
-]).purgeCss({
-    enabled: mix.inProduction(),
-    folders: ['src', 'templates', 'resources/views'],
-    extensions: ['html', 'js', 'php', 'vue'],
-});
+]);
